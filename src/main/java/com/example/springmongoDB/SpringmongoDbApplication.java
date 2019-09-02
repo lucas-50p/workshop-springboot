bcee83056a73f21d277a95b2ec4752dc4fa076de
Lucas Bueno <lucas-50p>
2019-09-02 18:51:18 -0300

    Primeiro commit - Projeto criado

---
+++ src/main/java/com/example/springmongoDB/SpringmongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmongoDbApplication.class, args);
+	}
+
+}
