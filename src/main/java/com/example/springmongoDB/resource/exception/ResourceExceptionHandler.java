91af7b21176f8d5349fca03aee7cb420c0ac3316
Lucas Bueno <lucas-50p>
2019-09-03 20:13:26 -0300

    Obtendo um usuario por id

---
+++ src/main/java/com/example/springmongoDB/resource/exception/ResourceExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.example.springmongoDB.resource.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.example.springmongoDB.services.exception.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandartError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+		
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandartError err = new StandartError(System.currentTimeMillis(), status.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+
+}
--- src/main/java/com/example/springmongoDB/services/UserService.java
+++ src/main/java/com/example/springmongoDB/services/UserService.java
@@ -1,12 +1,14 @@
 package com.example.springmongoDB.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.springmongoDB.domain.User;
 import com.example.springmongoDB.repository.UserRepository;
+import com.example.springmongoDB.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,5 +20,9 @@ public class UserService {
 		return repo.findAll();
 		
 	}
-
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
 }
