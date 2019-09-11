ceace837aa3cfe398dee3482ad082fd9460a6fd2
Lucas Bueno <lucas-50p>
2019-09-10 21:51:49 -0300

    Consulta simples com @Query

--- src/main/java/com/example/springmongoDB/repository/PostRepository.java
+++ src/main/java/com/example/springmongoDB/repository/PostRepository.java
@@ -3,13 +3,16 @@ package com.example.springmongoDB.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.springmongoDB.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String Text);//ingnorar letras maiusculas e minusculas
 }
